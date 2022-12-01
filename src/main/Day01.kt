a13bb8978444eb0a332a0e933ea5c4ff134f26dd
Simon Siegler <simon.siegler@gmail.com>
2022-12-01 08:17:41 +0100

    Solve part 1

--- src/main/Day01.kt
+++ src/main/Day01.kt
@@ -1,11 +1,25 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+data class Reader(val elves: List<List<Int>> = listOf(), val elf: List<Int> = listOf()) {
+    fun read(line: String): Reader =
+        when {
+            line.isBlank() -> Reader(complete())
+            else -> Reader(elves, elf + line.toInt())
+        }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun complete(): List<List<Int>> =
+        when {
+            elf.isEmpty() -> elves
+            else -> elves + listOf(elf)
+        }
+}
+
+fun part1(input: List<String>): Int =
+    input.fold(Reader(), Reader::read).complete().maxOf { it.sum() }
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
 
     val input = readInput("Day01")
     println(part1(input))
---
+++ src/test/Day01Test.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day01Test {
+    @Test
+    fun `reads calories by elf`() {
+        val calories: List<List<Int>> =
+            readInput("Day01Example").fold(Reader(), Reader::read).complete()
+
+        assertEquals(
+            listOf(
+                listOf(1000, 2000, 3000),
+                listOf(4000),
+                listOf(5000, 6000),
+                listOf(7000, 8000, 9000),
+                listOf(10000),
+            ),
+            calories
+        )
+    }
+
+    @Test
+    fun `find max calories sum`() {
+        assertEquals(24000, part1(readInput("Day01Example")))
+    }
+}
