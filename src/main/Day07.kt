58524c027d2ee894374d935fbc5683b15f8a1c14
Simon Siegler <simon.siegler@gmail.com>
2022-12-07 19:07:42 +0100

    Solve day07 part1

---
+++ src/main/Day07.kt
@@ -0,0 +1,80 @@
+package day07
+
+import utils.readInput
+
+typealias Path = List<String>
+
+data class File(val name: String, val size: Long)
+
+data class Directory(
+    val name: String,
+    private val files: MutableList<File> = mutableListOf(),
+    private val subdirectories: MutableList<Directory> = mutableListOf(),
+) {
+    fun addDirectory(directory: Directory) {
+        subdirectories.add(directory)
+    }
+
+    fun addFile(file: File) {
+        files.add(file)
+    }
+
+    private val totalSize: Long
+        get() = files.sumOf(File::size) + subdirectories.sumOf(Directory::totalSize)
+
+    val totalSizes: List<Long>
+        get() = subdirectories.flatMap(Directory::totalSizes) + totalSize
+}
+
+fun part1(filename: String): Long =
+    readInput(filename).readDirectories().totalSizes.filter { it <= 100000 }.sum()
+
+private val root = listOf("/")
+
+class DirectoryWalker(
+    private val directories: MutableMap<Path, Directory> = mutableMapOf(root to Directory("/")),
+    private var path: Path = emptyList()
+) {
+    fun execute(command: String) =
+        when {
+            command == "$ cd .." -> moveOut()
+            command.startsWith("$ cd ") -> moveIn(command.removePrefix("$ cd "))
+            command == "$ ls" -> {}
+            command.startsWith("dir ") -> addDirectory(command.removePrefix("dir "))
+            else -> {
+                val (size, name) = command.split(' ')
+                addFile(name, size.toLong())
+            }
+        }
+
+    private fun addFile(name: String, size: Long) {
+        currentDirectory().addFile(File(name, size))
+    }
+
+    private fun addDirectory(name: String) {
+        val directory = Directory(name)
+        directories[path + name] = directory
+        currentDirectory().addDirectory(directory)
+    }
+
+    private fun currentDirectory() = checkNotNull(directories[path]) { "Missing directory ${path}" }
+
+    private fun moveIn(subDirectory: String) {
+        path = path + subDirectory
+    }
+
+    private fun moveOut() {
+        path = path.subList(0, path.size - 1)
+    }
+
+    fun root(): Directory = checkNotNull(directories[root]) { "No root directory found" }
+}
+
+fun List<String>.readDirectories(): Directory =
+    DirectoryWalker().apply { forEach(::execute) }.root()
+
+const val filename = "Day07"
+
+fun main() {
+    println(part1(filename))
+}
---
+++ src/test/Day07Test.kt
@@ -0,0 +1,14 @@
+package day07
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+private const val filename: String = "Day07Example"
+
+class Day07Test {
+
+    @Test
+    fun part1() {
+        assertEquals(95437, part1(filename))
+    }
+}
