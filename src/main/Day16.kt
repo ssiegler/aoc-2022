feb6eabacda3300494dafd37acf4d8ed807302bf
Simon Siegler <simon.siegler@gmail.com>
2022-12-17 01:14:32 +0100

    Optimize part1

--- src/main/Day16.kt
+++ src/main/Day16.kt
@@ -1,46 +1,94 @@
 package day16
 
 import utils.readInput
+import java.util.*
+import kotlin.math.max
 
 data class Valves(
     private val flows: Map<String, Int>,
     private val tunnels: Map<String, List<String>>
 ) {
+    private val flowValves =
+        flows.entries.filter { (_, flow) -> flow > 0 }.map { (valve, _) -> valve }
 
-    private val cache = mutableMapOf<Triple<String, List<String>, Int>, Pair<Int, List<String>>>()
+    private val distances: Map<Pair<String, String>, Int> by lazy {
+        val distances = mutableMapOf<Pair<String, String>, Int>()
+        for (u in tunnels.keys) {
+            distances[u to u] = 0
+            for (v in tunnels[u] ?: emptyList()) {
+                distances[u to v] = 1
+            }
+        }
+        for (k in tunnels.keys) {
+            for (i in tunnels.keys) {
+                for (j in tunnels.keys) {
+                    val whole = distances[i to j]
+                    val first = distances[i to k]
+                    val second = distances[k to j]
+                    if (
+                        first != null && second != null && (whole == null || whole > first + second)
+                    ) {
+                        distances[i to j] = first + second
+                    }
+                }
+            }
+        }
 
-    fun maxPressure(
-        start: String = "AA",
-        minutes: Int = 30,
-        openValves: List<String> = emptyList(),
-        accumulatedFlow: Int = 0,
-    ): Pair<Int, List<String>> {
-        if (minutes <= 0) return accumulatedFlow to openValves
-
-        val key = Triple(start, openValves, minutes)
-        return cache[key]
-            ?: findMaxPressure(start, openValves, minutes, accumulatedFlow).also { cache[key] = it }
+        return@lazy distances.filterKeys { (_, dest) -> dest in flowValves }
     }
 
-    private fun findMaxPressure(
-        start: String,
-        openValves: List<String>,
-        minutes: Int,
-        accumulatedFlow: Int
-    ): Pair<Int, List<String>> =
-        (if (flows[start]!! > 0 && !openValves.contains(start)) {
-                tunnels[start]!!.map {
-                    maxPressure(
-                        it,
-                        minutes - 2,
-                        openValves + start,
-                        accumulatedFlow + flows[start]!! * (minutes - 1)
+    data class State(
+        val remaining: Int,
+        val position: String,
+        val openValves: List<String>,
+        val rate: Int,
+        val total: Int
+    ) {
+        fun flow(): Int = total + remaining * rate
+    }
+
+    fun maximalPressure(): Int {
+        var maxFlow = 0
+        val queue = ArrayDeque<State>()
+        queue.push(State(30, "AA", emptyList(), 0, 0))
+        while (queue.isNotEmpty()) {
+            val state = queue.pop()
+            maxFlow = max(maxFlow, state.flow())
+            if (state.remaining >= 0) {
+                val rate = flows[state.position]!!
+                if (state.position !in state.openValves && rate > 0) {
+                    queue.push(
+                        State(
+                            state.remaining - 1,
+                            state.position,
+                            state.openValves + state.position,
+                            state.rate + rate,
+                            state.total + state.rate
+                        )
                     )
+                } else {
+                    for (destination in flowValves) {
+                        if (destination !in state.openValves) {
+                            val distance = distances[state.position to destination]!!
+                            val remaining = state.remaining - distance
+                            if (remaining > 1) {
+                                queue.push(
+                                    State(
+                                        remaining,
+                                        destination,
+                                        state.openValves,
+                                        state.rate,
+                                        state.total + (state.rate * distance)
+                                    )
+                                )
+                            }
+                        }
+                    }
                 }
-            } else {
-                emptyList()
-            } + tunnels[start]!!.map { maxPressure(it, minutes - 1, openValves, accumulatedFlow) })
-            .maxBy { (flow, _) -> flow }
+            }
+        }
+        return maxFlow
+    }
 }
 
 fun List<String>.readValves(): Valves {
@@ -54,7 +102,7 @@ fun List<String>.readValves(): Valves {
     return Valves(flows, tunnels)
 }
 
-fun part1(filename: String) = readInput(filename).readValves().maxPressure().first
+fun part1(filename: String) = readInput(filename).readValves().maximalPressure()
 
 private const val filename = "Day16"
 
