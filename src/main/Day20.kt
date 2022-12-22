c745aa179b0fd8de9340ca32c06103a390078246
Simon Siegler <simon.siegler@gmail.com>
2022-12-22 16:37:49 +0100

    Solve day20 part 2

--- src/main/Day20.kt
+++ src/main/Day20.kt
@@ -4,19 +4,22 @@ import utils.readInput
 
 private const val filename = "Day20"
 
-fun part1(filename: String): Int =
-    readInput(filename).map { it.toInt() }.mix().grooveCoordinates().sum()
+fun part1(filename: String): Long = readEncrypted(filename).mix().grooveCoordinates().sum()
 
-fun part2(filename: String): Int = TODO()
+fun readEncrypted(filename: String, key: Long = 1) = readInput(filename).map { it.toLong() * key }
 
-fun List<Int>.grooveCoordinates(): List<Int> {
+fun part2(filename: String): Long =
+    readEncrypted(filename, 811589153).mix(10).grooveCoordinates().sum()
+
+fun List<Long>.grooveCoordinates(): List<Long> {
     val start = indexOf(0)
     return listOf(1000, 2000, 3000).map { this[(it + start) % size] }
 }
 
-fun List<Int>.mix(): List<Int> = withIndex().toMutableList().apply { mix() }.map { it.value }
+fun List<Long>.mix(times: Int = 1): List<Long> =
+    withIndex().toMutableList().apply { repeat(times) { mix() } }.map { it.value }
 
-private fun MutableList<IndexedValue<Int>>.mix() {
+private fun MutableList<IndexedValue<Long>>.mix() {
     indices.forEach { originalIndex ->
         val index = indexOfFirst { it.index == originalIndex }
         val element = removeAt(index)
--- src/test/Day20Test.kt
+++ src/test/Day20Test.kt
@@ -1,7 +1,7 @@
 import day20.grooveCoordinates
 import day20.mix
+import day20.readEncrypted
 import org.junit.jupiter.api.Test
-import utils.readInput
 import kotlin.test.assertEquals
 
 private const val filename = "Day20Example"
@@ -9,19 +9,21 @@ private const val filename = "Day20Example"
 class Day20Test {
     @Test
     fun `reads input`() {
-        assertEquals(listOf(1, 2, -3, 3, -2, 0, 4), readInput(filename).map { it.toInt() })
+        assertEquals(listOf<Long>(1, 2, -3, 3, -2, 0, 4), readEncrypted(filename))
     }
 
     @Test
     fun `groove coordinates`() {
-        assertEquals(
-            listOf(4, -3, 2),
-            readInput(filename).map { it.toInt() }.mix().grooveCoordinates()
-        )
+        assertEquals(listOf<Long>(4, -3, 2), readEncrypted(filename).mix().grooveCoordinates())
     }
 
     @Test
     fun part1() {
         assertEquals(3, day20.part1(filename))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(1623178306, day20.part2(filename))
     }
 }
